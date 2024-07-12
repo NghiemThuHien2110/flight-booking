d5fb4358b4857b6eba67e10901444f18e6bff339
lilynghiem <lily.nghiem@contemi.com>
2024-07-12 13:28:23 +0700

    add browser maximizeWindow

--- test/specs/flight.booking.js
+++ test/specs/flight.booking.js
@@ -4,6 +4,7 @@ import flightPage from '../pageobjects/flight.page.js';
 describe('Search for flights', () => {
     it('should booking flight success', async () => {
         await HomePage.open();
+        await browser.maximizeWindow();
         await HomePage.selectFlightFromTo("HAN", "SGN");
         await HomePage.selectFlightDate();
         await HomePage.selecPassengers(1, 1);
